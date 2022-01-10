30cbdd0573b92725278074fbc38435c5c3604fa7
박성재 <bobsot0225@gmail.com>
2022-01-10 23:05:10 +0900

    2022. 01. 10. Vue3 Lecture Day2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -10,12 +10,17 @@
         </div>
         <div id="contentsBox">
           <div class="buttonBox">
-            <button class="forecast">Forecast</button>
-            <button class="airquality">Air Quality</button>
+            <div class="buttonBackground">
+              <button class="forecast">Forecast</button>
+              <button class="airquality">Air Quality</button>
+            </div>
           </div>
           <div class="weatherBox">
+            <div class="weatherDegree">
+              <p>10&deg;</p>
+            </div>
             <div class="weatherIcon">
-              <img src="~/assets/storm.png" alt="" />
+              <img src="~/assets/43.png" alt="" />
             </div>
             <div class="weatherData">
               <div v-for="weatherValue in weather" :key="weatherValue" class="detailData">
@@ -27,17 +32,21 @@
         </div>
         <div id="todayWeather">
           <div class="textBox">
-            <p>Today</p>
-            <p>View full report</p>
+            <p>시간대별 날씨 정보</p>
+            <p>이번주 날씨 보기</p>
           </div>
           <div class="timelyWeatherBox">
             <div class="timelyWeather">
               <div class="icon">
-                <img src="~/assets/sunshine.png" alt="" />
+                <img src="~/assets/29.png" alt="" />
               </div>
               <div class="data">
-                <p>14:00</p>
-                <p>32</p>
+                <p class="time">2pm</p>
+                <p class="currentDegree">32&deg;</p>
+                <div>
+                  <img src="~/assets/drop.png" alt="" />
+                  <p class="fall">15%</p>
+                </div>
               </div>
             </div>
           </div>
@@ -60,16 +69,16 @@
       return {
         weather: [
           {
-            title: 'Temp',
-            value: '32',
+            title: '습도',
+            value: '88%',
           },
           {
-            title: 'Wind',
+            title: '풍속',
             value: '10m/s',
           },
           {
-            title: 'Humidity',
-            value: '50%',
+            title: '풍향',
+            value: 'WS',
           },
         ],
       };
@@ -87,94 +96,141 @@
 
     .container {
       @include center-sb;
-      width: 65%;
+      width: 55%;
       height: 90%;
       max-width: 1920px;
       max-height: 1354px;
 
       // 공통 코드이기 때문에 어떻게 클린하게 작성할 수 있을지 고민해보자.
       .leftContainer {
-        width: 40%;
-        height: 100%;
+        // width: 40%;
+        // height: 100%;
+        width: 324px;
+        height: 700px;
         border-radius: 50px;
-        background-color: $primary;
+        background: linear-gradient(#16455f, #0e1239);
+
         box-shadow: 5px 5px 10px gray;
 
         #cityNameBox {
-          @include center;
           width: 100%;
-          height: 15%;
-
-          p {
+          height: 10%;
+          .cityName {
+            @include c-center;
             width: 100%;
-            color: white;
-            font-family: 'Poppins', sans-serif;
-            line-height: 1.5;
+            height: 100%;
 
-            &:first-child {
-              font-size: 1.5rem;
-            }
-            &:last-child {
-              font-size: 0.9rem;
-              font-weight: 200;
+            p {
+              color: white;
+              font-family: 'Poppins', sans-serif;
+              line-height: 2.5;
               text-align: center;
+
+              &:first-child {
+                width: 241px;
+                height: 33px;
+                font-size: 1.35rem;
+              }
+              &:last-child {
+                width: 160px;
+                height: 19px;
+                font-size: 0.9rem;
+                font-weight: 100;
+              }
             }
           }
         }
         #contentsBox {
           width: 100%;
-          height: 55%;
+          height: 62.5%;
           .buttonBox {
             @include center;
             width: 100%;
             height: 20%;
 
-            button {
-              width: 125px;
+            .buttonBackground {
+              width: 224px;
               height: 35px;
-              margin: 0 10px;
-              border: 0;
-              border-radius: 7.5px;
-              outline: 0;
-              background-color: rgba(245, 245, 245, 0.5);
-              cursor: pointer;
-              box-shadow: 1px 1px 3px gray;
+              background-color: #052137;
+              border-radius: 10px;
+              display: flex;
+
+              button {
+                width: 112px;
+                height: 35px;
+                border: 0;
+                border-radius: 7.5px;
+                outline: 0;
+                cursor: pointer;
+                &.forecast {
+                  background-color: #0889ff;
+                  color: white;
+                }
+                &.airquality {
+                  background: transparent;
+                  color: #467599;
+                }
+              }
             }
           }
           .weatherBox {
             width: 100%;
             height: 80%;
 
+            .weatherDegree {
+              @include center;
+              width: 100%;
+              height: 15%;
+
+              p {
+                font-size: 3.5rem;
+                font-weight: 500;
+                font-family: 'Be Vietnam Pro', sans-serif;
+                color: #fff;
+              }
+            }
             .weatherIcon {
               @include center;
               width: 100%;
-              height: 70%;
+              height: 60%;
 
               img {
-                height: 80%;
+                width: 168px;
+                height: 160px;
               }
             }
             .weatherData {
               display: flex;
               width: 100%;
-              height: 30%;
-
+              height: 25%;
               .detailData {
                 @include c-center;
                 width: 33.33%;
                 height: 100%;
+
+                &:nth-child(1) {
+                  margin-left: 10px;
+                }
+                &:nth-child(2) {
+                  margin: 0 -10px;
+                }
+                &:nth-child(3) {
+                  margin-right: 10px;
+                }
 
                 p {
                   line-height: 1.5;
                   color: white;
 
                   &:first-child {
+                    font-family: 'Noto Sans KR', sans-serif;
                     font-size: 1rem;
-                    font-weight: 200;
+                    font-weight: 300;
+                    color: #799ed0;
                   }
                   &:last-child {
                     font-family: 'Poppins', sans-serif;
-                    font-size: 1.3rem;
+                    font-size: 1rem;
                     font-weight: 200;
                   }
                 }
@@ -184,67 +240,81 @@
         }
         #todayWeather {
           width: 100%;
-          height: 20%;
+          height: 17.5%;
           .textBox {
             @include center-sb;
-            width: calc(100% - 75px);
-            height: 40%;
-            padding: 0 35px 0 40px;
+            width: calc(100% - 70px);
+            height: 35%;
+            padding: 0 35px;
+            font-family: 'Noto Sans KR', sans-serif;
 
             p {
-              font-family: 'Poppins', sans-serif;
-              font-weight: 700;
-              font-size: 1.2rem;
+              font-weight: 400;
+              font-size: 0.8rem;
               color: white;
+              text-align: center;
 
               &:last-child {
-                font-weight: 200;
+                font-weight: 400;
                 font-size: 0.8rem;
-                color: skyblue;
+                color: #0085ff;
                 cursor: pointer;
+                margin-bottom: 2px;
               }
             }
           }
           .timelyWeatherBox {
             display: flex;
             align-items: center;
-            width: calc(100% -80px);
-            height: 60%;
-            padding: 0 40px;
+            width: calc(100% - 70px);
+            height: 65%;
+            padding: 0 30px;
             .timelyWeather {
               display: flex;
-              width: 50%;
-              height: 90%;
-              background-color: rgba(189, 224, 255, 0.3);
-              border-radius: 10px;
+              width: 126px;
+              height: 70px;
+              background-color: #0989ff;
+              border-radius: 20px;
               .icon {
                 @include center;
-                width: 50%;
+                width: 45%;
                 height: 100%;
 
                 img {
-                  height: 80%;
+                  width: 100%;
                 }
               }
               .data {
-                @include c-center;
-                width: 50%;
+                @include c-center-se;
+                width: 55%;
                 height: 100%;
 
                 p {
                   color: whitesmoke;
                   font-family: 'Poppins', sans-serif;
-                  margin-top: 10px;
-
-                  &:first-child {
-                    @include center;
-                    font-size: 1rem;
+                  text-align: center;
+                  &.time {
+                    font-size: 0.8rem;
                     font-weight: 200;
-                    margin-top: 5px;
+                    margin-top: 7.5px;
                   }
-                  &:last-child {
-                    @include center;
-                    font-size: 1.5rem;
+                  &.currentDegree {
+                    font-size: 1.2rem;
+                    margin-top: 7.5px;
+                  }
+                }
+
+                div {
+                  @include center;
+                  width: 100%;
+                  height: 33.33%;
+
+                  img {
+                    height: 55%;
+                  }
+                  .fall {
+                    font-size: 0.9rem;
+                    margin-top: 1.5px;
                   }
                 }
               }
@@ -253,9 +323,9 @@
         }
         nav {
           @include center-sb;
-          width: calc(100% - 78px);
+          width: calc(100% - 100px);
           height: 10%;
-          padding: 0 40px;
+          padding: 0 50px;
 
           i {
             color: rgb(255, 255, 255);
@@ -264,16 +334,18 @@
             transition: all 0.2s ease;
 
             &:hover {
-              color: rgb(75, 224, 133);
+              color: #799ed0;
             }
           }
         }
       }
       .rightContainer {
-        width: 40%;
-        height: 100%;
+        // width: 40%;
+        //height: 100%;
+        width: 324px;
+        height: 700px;
         border-radius: 50px;
-        background-color: $primary;
+        background: linear-gradient(#16455f, #0e1239);
         box-shadow: 5px 5px 10px gray;
       }
     }
