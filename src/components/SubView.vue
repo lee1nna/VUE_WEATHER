b3fb92b22ce0a1ea0a4e67a61dba38b14bd96f4f
박성재 <bobsot0225@gmail.com>
2022-01-12 23:47:14 +0900

    2022. 01. 12. Vue3 Lecture Day3

--- src/components/SubView.vue
+++ src/components/SubView.vue
@@ -12,6 +12,22 @@
         <div class="airCondition">
           <p>매우 나쁨</p>
         </div>
+        <div class="detail">
+          <div class="title">
+            <p>오염물질</p>
+          </div>
+          <div v-for="airCondition in airConditions" :key="airCondition" class="data">
+            <div class="dataName">
+              <p></p>
+              <p>{{ airCondition.air }}</p>
+            </div>
+            <div class="dataValue">
+              <p>
+                <span>{{ airCondition.value }}</span> &#181;g/m&#179;
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -19,7 +35,40 @@
 
 <script>
   import CityNameBox from './common/CityNameBox.vue';
-  export default { components: { CityNameBox } };
+
+  export default {
+    components: { CityNameBox },
+    data() {
+      return {
+        airConditions: [
+          {
+            air: '초미세먼지',
+            value: 65,
+          },
+          {
+            air: '미세먼지',
+            value: 74,
+          },
+          {
+            air: '오존',
+            value: 16,
+          },
+          {
+            air: '이산화질소',
+            value: 103.4,
+          },
+          {
+            air: '이산화황',
+            value: 10.5,
+          },
+          {
+            air: '일산화탄소',
+            value: 916,
+          },
+        ],
+      };
+    },
+  };
 </script>
 
 <style lang="scss" scoped>
