2098247e7caae0ec2fe596d7cc90e23ddd419fb8
박성재 <bobsot0225@gmail.com>
2022-01-12 21:51:30 +0900

    2022. 01. 12. Vue3 Lecture Day3

---
+++ src/components/MainView.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="leftContainer">
+    <div id="cityNameBox">
+      <div class="cityName">
+        <p>San Fransisco</p>
+        <p>Jan 28, 2022</p>
+      </div>
+    </div>
+    <div id="contentsBox">
+      <div class="buttonBox">
+        <div class="buttonBackground">
+          <button class="forecast">Forecast</button>
+          <button class="airquality">Air Quality</button>
+        </div>
+      </div>
+      <div class="weatherBox">
+        <div class="weatherDegree">
+          <p>10&deg;</p>
+        </div>
+        <div class="weatherIcon">
+          <img src="~/assets/43.png" alt="" />
+        </div>
+        <div class="weatherData">
+          <div v-for="weatherValue in weather" :key="weatherValue" class="detailData">
+            <p>{{ weatherValue.title }}</p>
+            <p>{{ weatherValue.value }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div id="todayWeather">
+      <div class="textBox">
+        <p>시간대별 날씨 정보</p>
+        <p>이번주 날씨 보기</p>
+      </div>
+      <div class="timelyWeatherBox">
+        <div class="timelyWeather">
+          <div class="icon">
+            <img src="~/assets/29.png" alt="" />
+          </div>
+          <div class="data">
+            <p class="time">2pm</p>
+            <p class="currentDegree">32&deg;</p>
+            <div>
+              <img src="~/assets/drop.png" alt="" />
+              <p class="fall">15%</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <nav>
+      <i class="fas fa-home"></i>
+      <i class="fas fa-search-location"></i>
+      <i class="fas fa-chart-line"></i>
+      <i class="fas fa-cog"></i>
+    </nav>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        weather: [
+          {
+            title: '습도',
+            value: '88%',
+          },
+          {
+            title: '풍속',
+            value: '10m/s',
+          },
+          {
+            title: '풍향',
+            value: 'WS',
+          },
+        ],
+      };
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  @import '~/scss/main.scss';
+  @import '~/scss/mainView.scss';
+</style>
---
+++ src/components/SubView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="rightContainer"></div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  @import '~/scss/main.scss';
+  @import '~/scss/subView.scss';
+</style>
