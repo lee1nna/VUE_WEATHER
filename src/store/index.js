1986de9bc7f6c68e63988800fc4ced67e74f610e
박성재 <bobsot0225@gmail.com>
2022-02-11 09:36:55 +0900

    2022. 02. 11. Vue3 Lecture Day8

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,12 @@
+// Vuex 설치 : npm install vuex@next
+// 객체 구조분해를 통해서
 import { createStore } from 'vuex';
-import onedayData from './dayApi';
+import COMMON1 from './common1'; // city weather api - store
+import COMMON2 from './common2'; // timely weather api - store
 
 export default createStore({
   modules: {
-    onedayData,
+    COMMON1,
+    COMMON2,
   },
 });
